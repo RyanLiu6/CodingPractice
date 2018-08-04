3195919cf26172e3593f157af2ed8c4b93ab95b9
Ryan Liu <dio.ryanliu@hotmail.com>
2018-08-03 20:58:58 -0700

    Updating stuff

--- Python/Learn/HashTable/Key.py
+++ Python/Learn/HashTable/Key.py
@@ -1,3 +1,10 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
 class KeySolution:
     def groupAnagrams(self, strs):
         """
@@ -21,9 +28,81 @@ class KeySolution:
                 index += 1
 
         return retList
-        
+
     def isValidSudoku(self, board):
         """
         :type board: List[List[str]]
         :rtype: bool
+        Input:
+        [
+          ["5","3",".",".","7",".",".",".","."],
+          ["6",".",".","1","9","5",".",".","."],
+          [".","9","8",".",".",".",".","6","."],
+          ["8",".",".",".","6",".",".",".","3"],
+          ["4",".",".","8",".","3",".",".","1"],
+          ["7",".",".",".","2",".",".",".","6"],
+          [".","6",".",".",".",".","2","8","."],
+          [".",".",".","4","1","9",".",".","5"],
+          [".",".",".",".","8",".",".","7","9"]
+        ]
+        Output: true
         """
+        return self.isRowValid(board) and self.isColValid(board) and self.isSquareValid(board)
+
+    def isRowValid(self, board):
+        rowDict = {}
+        for row in board:
+            for c in row:
+                if c != "." and c in rowDict:
+                    return False
+                else:
+                    rowDict[c] = 1
+            rowDict.clear()
+        return True
+
+    def isColValid(self, board):
+        colDict = {}
+        for i in range(len(board[0])):
+            for row in board:
+                if row[i] != "." and row[i] in colDict:
+                    return False
+                else:
+                    colDict[row[i]] = 1
+            colDict.clear()
+        return True
+
+    def isSquareValid(self, board):
+        squareDict = {}
+        for i in (0, 3, 6):
+            for j in (0, 3, 6):
+                square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+                for c in square:
+                    if c != "." and c in squareDict:
+                        return False
+                    else:
+                        squareDict[c] = 1
+                squareDict.clear()
+        return True
+
+    def findDuplicateSubtrees(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[TreeNode]
+        """
+        treeDict = {}
+        retSet = set()
+
+        def dupHelper(root):
+            if not root:
+                return
+
+            if root.val in treeDict:
+                retSet.add(root)
+            else:
+                treeDict[root.val] = 1
+
+            dupHelper(root.left)
+            dupHelper(root.right)
+
+        dupHelper(root)
+        return list(retSet)
