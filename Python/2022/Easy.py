e8c46900c4c48494fdcd2348d37595ab2b4752a7
Ryan Liu <ryan@ryanliu6.xyz>
2022-03-31 11:37:14 -0700

    Easy #21

--- Python/2022/Easy.py
+++ Python/2022/Easy.py
@@ -1,4 +1,12 @@
-from typing import List
+from typing import List, Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 
 class Solution:
     def two_sum(self, nums: List[int], target: int) -> List[int]:
@@ -161,5 +169,87 @@ class Solution:
         else:
             return True
 
+    def merge_two_lists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Leetcode Problem #21
+
+        LinkedList problems often needs a root node and a current node to iterate.
+
+        Input: list1 = [1,2,4], list2 = [1,3,4]
+        Output: [1,1,2,3,4,4]
+        """
+
+        # Iterative
+        if not list1 and list2:
+            return list2
+        elif not list2 and list1:
+            return list1
+        elif not list1 and not list2:
+            return None
+
+        merge_root: ListNode = None
+        if list1.val < list2.val:
+            merge_root = list1
+            list1 = list1.next
+        else:
+            merge_root = list2
+            list2 = list2.next
+
+        # Iterate until one is empty
+        current: ListNode = merge_root
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+
+            current = current.next
+
+        # Check if there's anything leftover
+        if list1:
+            current.next = list1
+        else:
+            current.next = list2
+
+        return merge_root
+
+        # Recursive
+        """
+        Build base case - if either is None, we return the other
+
+        Small case:
+        list1 = [1]
+        list2 = [2]
+        output = [1, 2]
+
+        More complex case:
+        list1 = [1, 3]
+        list2 = [2, 4]
+        output = [1, 2, 3, 4]
+        """
+        if not list1 and list2:
+            return list2
+        elif not list2 and list1:
+            return list1
+        elif not list1 and not list2:
+            return None
+
+        if list1.val < list2.val:
+            list1.next = self.merge_two_lists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.merge_two_lists(list1, list2.next)
+            return list2
+
 solution = Solution()
-print(solution.is_valid("()"))
+
+list1 = ListNode(1, next=ListNode(2, next=ListNode(4)))
+list2 = ListNode(1, next=ListNode(3, next=ListNode(4)))
+
+result = solution.merge_two_lists(list1, list2)
+
+while result.next:
+    print(result.val)
+    result = result.next
